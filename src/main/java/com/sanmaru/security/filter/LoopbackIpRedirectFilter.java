2a68aec526f7da40391c55dd685ded1256e81a59
archrua <moni@chosunbiz.com>
2021-12-10 19:30:27 +0900

    소스 정리

--- src/main/java/com/sanmaru/security/filter/LoopbackIpRedirectFilter.java
+++ src/main/java/com/sanmaru/security/filter/LoopbackIpRedirectFilter.java
@@ -4,8 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -26,6 +31,16 @@ public class LoopbackIpRedirectFilter extends OncePerRequestFilter {
                 + request.getMethod() + " "
                 + request.getRequestURI()
                 + ( request.getQueryString() != null ? "?" + request.getQueryString() : "" ));
+        CsrfToken token = new HttpSessionCsrfTokenRepository().loadToken(request);
+        if (token != null) {
+            logger.info("======= " + "CsrfToken : " + token.getToken());
+        }
+//        if (request.getServerName().equals("localhost") && request.getHeader("host") != null) {
+//            UriComponents uri = UriComponentsBuilder.fromHttpRequest(new ServletServerHttpRequest(request))
+//                    .host("127.0.0.1").build();
+//            filterChain.doFilter(request, response);
+//            response.sendRedirect(uri.toUriString());
+//        }
         filterChain.doFilter(request, response);
     }
 
